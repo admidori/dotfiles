caa052c04d058e64cda8db77781b938820359443
Aoi Kondo <agotadmidori@gmail.com>
2022-02-18 22:01:02 +0900

    complete task. I'll do debug...

---
+++ debian/bin/vscode.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update
+sudo apt-get -y install apt-transport-https
+sudo apt update
+sudo apt install code
