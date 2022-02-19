feaf081d88f780d374b3a4255442db2fb73c4424
Aoi Kondo <agotadmidori@gmail.com>
2022-02-19 13:00:02 +0900

    fix bug

--- debian/bin/docker.sh
+++ debian/bin/docker.sh
@@ -6,8 +6,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
      "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
       $(lsb_release -cs) \
-         stable")
+         stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
 sudo usermod -aG docker $(whoami)
-sudo apt install docker-compose
+sudo apt install -y docker-compose
