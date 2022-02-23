7846081be23c2df114a1db8d070b6802b9c8dd12
Aoi Kondo <agotadmidori@gmail.com>
2022-02-23 12:56:29 +0900

    Update file: update docker-compose

--- debian/bin/tools/docker.sh
+++ debian/bin/tools/docker.sh
@@ -10,4 +10,5 @@ sudo add-apt-repository \
 sudo apt-get update
 sudo apt-get install -y docker-ce
 sudo usermod -aG docker $(whoami)
-sudo apt install -y docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
