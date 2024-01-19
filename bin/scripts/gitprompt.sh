980afc2fcbf988c455520ac10f81fb40096e2048
Aoi Kondo <agotadmidori@gmail.com>
2024-01-19 09:45:48 +0900

    Add docker installiing script

--- bin/scripts/gitprompt.sh
+++ bin/scripts/gitprompt.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 mkdir -P ../binscripts/git-branch
 wget -P ../binscripts/git-branch https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 wget -P ../binscripts/git-branch https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
