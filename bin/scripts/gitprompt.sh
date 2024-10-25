ff81f1f2591c6e1cec864913fc2b760de4f6ef57
Aoi Kondo <agotadmidori@gmail.com>
2024-10-25 18:48:19 +0900

    Update install script
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

--- bin/scripts/gitprompt.sh
+++ bin/scripts/gitprompt.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mkdir -P ../binscripts/git-branch
-wget -P ../binscripts/git-branch https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-wget -P ../binscripts/git-branch https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+mkdir -P ../../vendor/git-branch
+wget -P ../../vendor/git-branch https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+wget -P ../../vendor/git-branch https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
