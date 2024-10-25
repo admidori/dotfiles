2d1a1155525e6775a4b814faf904897028a23342
Aoi Kondo <agotadmidori@gmail.com>
2024-10-25 20:47:31 +0900

    Update install script
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

--- installer/link.sh
+++ installer/link.sh
@@ -3,6 +3,14 @@
 echo "########################"
 echo "#  MAKE SYMBOLIC LINK  #"
 echo "########################"
+cd ~/
+for f in .??*
+do
+  if [ -L $f ]; then
+    unlink ./$f
+  fi
+done
+
 cd $MAIN_PATH/bin/dotfiles
 for f in .??*
 do
