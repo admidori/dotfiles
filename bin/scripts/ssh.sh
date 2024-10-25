6cbb1f6dbdaadbf7a659f4e571a47bf7035e18fb
Aoi Kondo <agotadmidori@gmail.com>
2024-10-25 20:48:14 +0900

    Update install script
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

--- bin/scripts/ssh.sh
+++ bin/scripts/ssh.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-sudo apt install openssh-server openssh-client
+sudo apt update
+sudo apt install -y openssh-server openssh-client
 ssh-keygen -t ed25519 -N ""
 ssh-add ~/.ssh/id_ed25519
