d8dde71b94732420521f484dfe713bc127aac672
Aoi Kondo <agotadmidori@gmail.com>
2024-10-26 11:09:09 +0900

    Update script
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

--- bin/dotfiles/.config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ bin/dotfiles/.config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -208,7 +208,9 @@ return {
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
+        'lua_ls', -- Lua
+        'clangd', -- C, C++
+        'gopls', -- Golang
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
