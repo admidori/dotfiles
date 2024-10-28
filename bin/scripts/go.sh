0437c873c8afa0228191053bc1a117f13e50c22f
Aoi Kondo <agotadmidori@gmail.com>
2024-10-28 18:18:29 +0900

    Add a installation script

---
+++ bin/scripts/go.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt-get install gccgo-5
+sudo update-alternatives --set go /usr/bin/go-5
+GOROOT_BOOTSTRAP=/usr ./make.bash
+
+git clone https://github.com/golang/go.git ../tmp
+cd ../tmp/go
+git checkout `git describe --tags --abbrev=0`
+cd src
+sudo chmod 777 all.bash
+./all.bash
+
